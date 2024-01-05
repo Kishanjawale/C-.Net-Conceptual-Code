62fd24cdcdcead0aaf3e680bbcf76fe8e6729c10
KISHAN JAWALE <kishanjawale10@gmail.com>
2024-01-05 22:26:28 +0530

    Add files via upload

---
+++ Variables.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Variables
+{
+    static void Main()
+    {
+        int iNum=5;
+        double dDoubleNum=5.99D;
+        char cLetter='D';
+        bool bBoolean= true;
+        string Str="Hello";
+        Console.WriteLine("intger:"+iNum);
+        Console.WriteLine("\nDouble:"+dDoubleNum);
+        Console.WriteLine("\nCharacter"+cLetter);
+        Console.WriteLine("\nBoolean:"+bBoolean);
+        Console.WriteLine("\nString"+Str);
+    }
+}
