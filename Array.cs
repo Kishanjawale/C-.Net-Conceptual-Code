b6e80ad278e7aad4d37d1d2135e953e341a7f5df
KISHAN JAWALE <kishanjawale10@gmail.com>
2024-01-06 22:31:24 +0530

    Add files via upload

---
+++ Array.cs
@@ -0,0 +1,14 @@
+using System;
+
+class ArrayExample
+{
+    static void Main()
+    {
+        string [] Cars={"audi","bmw","volvo","Mahindra","Tata"};
+        for(int icnt=0;icnt<Cars.Length;icnt++)
+        {
+         Console.WriteLine(Cars[icnt]);                   
+        }
+
+    }
+}
