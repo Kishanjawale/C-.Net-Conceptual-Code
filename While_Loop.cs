6b727d994264aa11bbc89c67ecd0344bd936c97a
KISHAN JAWALE <kishanjawale10@gmail.com>
2024-01-06 12:05:33 +0530

    Add files via upload

---
+++ While_Loop.cs
@@ -0,0 +1,16 @@
+using System;
+
+class WhileLoop
+{
+    static void Main()
+    {
+        int icnt=0;
+        while(icnt  < 5)
+        {
+            Console.WriteLine("Jai Ganesh...");
+            icnt++;
+            
+        }
+
+    }
+}
