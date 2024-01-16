592d39bf6f389b2b5af016a7c5471c692d05b3ac
KISHAN JAWALE <kishanjawale10@gmail.com>
2024-01-16 09:09:08 +0530

    Update Exception_Handling.cs

--- Exception_Handling.cs
+++ Exception_Handling.cs
@@ -6,12 +6,13 @@ namespace Myapp
     {
         static void Main(string[] arg)
         {
+             # Try Block Contains Exception Prone code
             try
             {
                 int [] iNumbers={1,2,3};
-                Console.WriteLine(iNumbers[10]);
+                Console.WriteLine(iNumbers[10]); //This will generate array index out of bound exception.    
             }
-            catch(Exception e)
+            catch(Exception e) //Catch block to catch the exception.
             {
                 Console.WriteLine(e.Message);
             }
